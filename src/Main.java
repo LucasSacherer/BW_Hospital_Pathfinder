16ebb25ebe18e50ee65cbb616c001ed0e0f480e1
zdhalzel <zdhalzel@wpi.edu>
2017-11-05 22:43:52 -0500

    Started an unattractive UI, it's not as good as Gluon but it's work-able for now. This is just so everyone can see a starting point.

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("boundary/main.fxml"));
         primaryStage.setTitle("B&W Path Finding");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
     }
 
