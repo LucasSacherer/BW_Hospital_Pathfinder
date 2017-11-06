a6d9fbe4b18b5117a3f692bec7d75437653c31a9
zdhalzel <zdhalzel@wpi.edu>
2017-11-06 16:55:20 -0500

    Added a button

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("boundary/main.fxml"));
         primaryStage.setTitle("B&W Path Finding");
-        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
