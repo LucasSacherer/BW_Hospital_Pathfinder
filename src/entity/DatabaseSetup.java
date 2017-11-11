f0edf25f005c6f2a8dd6790d3ae98c25ff9a18db
Quinn Averill <quinn.averill@trinetx.com>
2017-11-11 17:21:10 -0500

    Database now inserts all edges and nodes

--- src/entity/DatabaseSetup.java
+++ src/entity/DatabaseSetup.java
@@ -1,11 +1,16 @@
 package entity;
 
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseSetup {
+    /**
+     * This method creates the NODE and EDGE tables in the database, and inserts the data from the databaseDB/text files
+     * @throws SQLException Throws this exception if the database has already been created on the machine
+     */
     public void createDatbase() throws SQLException {
         //DB Info
         String dbURL = "jdbc:derby://localhost:1527/bw_pathfinder_db;create=true;user=granite_gargoyle;password=wong";
@@ -43,7 +48,7 @@ public class DatabaseSetup {
                     " floor varchar(3) NOT NULL,\n" +
                     " building varchar(20) NOT NULL,\n" +
                     " nodeType varchar(10) NOT NULL,\n" +
-                    " longName varchar(30) NOT NULL,\n" +
+                    " longName varchar(50) NOT NULL,\n" +
                     " shortName varchar(20) NOT NULL,\n" +
                     " teamAssigned varchar(10) NOT NULL,\n" +
                     " visitable varchar(5) NOT NULL\n " +
@@ -52,27 +57,57 @@ public class DatabaseSetup {
             System.out.println ("Table already exists");
         }
 
-        //Insert all the nodes from the csv
+        //Insert all Nodes to the table
         try {
-            stmt.execute("CREATE TABLE node (\n" +
-                    " nodeID varchar(20) PRIMARY KEY,\n" +
-                    " xcoord INTEGER NOT NULL,\n" +
-                    " ycoord INTEGER NOT NULL,\n" +
-                    " floor varchar(3) NOT NULL,\n" +
-                    " building varchar(20) NOT NULL,\n" +
-                    " nodeType varchar(10) NOT NULL,\n" +
-                    " longName varchar(30) NOT NULL,\n" +
-                    " shortName varchar(20) NOT NULL,\n" +
-                    " teamAssigned varchar(10) NOT NULL,\n" +
-                    " visitable varchar(5) NOT NULL\n " +
-                    ")");
-        } catch (SQLException e){
-            System.out.println ("Table already exists");
+            insertStatementsFromFile("src/databaseData/nodeInserts.txt", stmt);
+        } catch (FileNotFoundException e) {
+            System.out.println( "The file src/databaseData/nodeInserts.txt does not exist!");
         }
 
+        //Insert all Edges to the table
+        try {
+            insertStatementsFromFile("src/databaseData/edgeInserts.txt", stmt);
+        } catch (FileNotFoundException e) {
+            System.out.println( "The file src/databaseData/edgeInserts.txt does not exist!");
+        }
 
+        //Close shit down
         System.out.println("Database Created!");
         stmt.close();
         conn.close();
+    }
+
+    /**
+     * This method takes a path to a text file of SQL statements and calls them using the stmt param
+     * @param path the file path to be accessed
+     * @param stmt the stmt to be executed
+     */
+    private void insertStatementsFromFile(String path, Statement stmt) throws FileNotFoundException {
+        File file = new File(path);
+        FileReader fileReader;
+
+        //Try to open file
+        try {
+            fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException();
+        }
+        BufferedReader br = new BufferedReader(fileReader);
+
+        //Try to read each line and execute it on the database
+        String line;
+        int failedRows = 0;
+        try {
+            while ((line = br.readLine()) != null){
+                try {
+                    stmt.execute(line);
+                } catch (SQLException e){
+                    failedRows++;
+                }
+            }
+            if (failedRows != 0){ System.out.println(failedRows + " rows already exist."); }
+        } catch (IOException e) {
+            System.out.println("Cannot read file!");
+        }
     }
 }
