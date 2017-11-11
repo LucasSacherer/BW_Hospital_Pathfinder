d94c899e19b6da0e239cec3c1cdfd5c9588cf309
Taylor Stephen <tastephen@wpi.edu>
2017-11-11 13:28:23 -0500

    Added Edges Object and Test Class

---
+++ tests/entity/EdgeTest.java
@@ -0,0 +1,17 @@
+package entity;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class EdgeTest {
+    @Test
+    public void testSetWeight () {
+        Node n1 = new Node(1,1,1,1,"Shapiro","Stairwell","STAI");
+        Node n2 = new Node(2,2,1,1,"Shapiro","Stairwell","STAI");
+        Edge e = new Edge(n1,n2);
+        assertEquals(e.getWeight(),1);
+    }
+
+}
