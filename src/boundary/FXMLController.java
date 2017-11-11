fcae50714299f046eb8afffc91a05ac45add11ef
zdhalzel <zdhalzel@wpi.edu>
2017-11-11 14:34:28 -0500

    Started writing the FXMLController class

--- src/boundary/FXMLController.java
+++ src/boundary/FXMLController.java
@@ -1,4 +1,17 @@
 package boundary;
 
+import entity.Node;
+
+import java.util.List;
+
 public class FXMLController {
+    private Node loc1;
+    private Node loc2;
+    private Node currentLoc;
+    // private image currentMap; // TODO
+    private int time;
+    private List < List <Node> > directory;
+    private int currentFloor;
+    private List <Node> currentPath;
+
 }
