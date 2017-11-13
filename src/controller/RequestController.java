50f663977aeffb1a2615b18df653888a052a8114
Sean Hunt <srhunt@wpi.edu>
2017-11-13 12:00:57 -0500

    Added Test class for Request Controller and getRequests() method

--- src/controller/RequestController.java
+++ src/controller/RequestController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import entity.RequestManager;
+import entity.Request;
+import java.util.List;
+
 public class RequestController {
+    final private RequestManager requestManager = new RequestManager();
+
+    //Check if this needs to be
+    public void addRequest(Request req){
+    //    getRequests().add(req);
+    }
+
+    protected void validateRequest(Request req){
+
+    }
+
+    /**
+     * Calls the getRequests() method in RequestManager
+     * @return The list of current requests in request attribute of RequestManager.
+     */
+    public List<Request> getRequests(){
+        return RequestManager.getRequests();
+    }
+
+    public void deleteRequest(Request req){
+
+    }
 }
---
+++ tests/controller/RequestControllerTest.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RequestControllerTest {
+    @Test
+    public void addRequest() throws Exception {
+    }
+
+    @Test
+    public void validateRequest() throws Exception {
+    }
+
+    @Test
+    public void getRequests() throws Exception {
+    }
+
+    @Test
+    public void deleteRequest() throws Exception {
+    }
+
+}
