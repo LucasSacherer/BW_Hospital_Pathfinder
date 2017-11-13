bae952045693a01f8a3fbcd7c132ee4137385816
Quinn Averill <qsaverill@wpi.edu>
2017-11-13 15:28:23 -0500

    Created MapManager Constructor

--- src/entity/MapManager.java
+++ src/entity/MapManager.java
@@ -1,4 +1,14 @@
 package entity;
 
+import java.awt.*;
+import java.util.HashMap;
+
 public class MapManager {
+    private HashMap<String, Image> maps;
+
+    public MapManager(HashMap<String, Image> maps) {
+        this.maps = maps;
+    }
+
+
 }
