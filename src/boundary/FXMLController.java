bbd4c8eac95d68a77615ee26e65bd37e89795870
dtsiakmakis <33322535+dtsiakmakis@users.noreply.github.com>
2017-11-14 00:42:46 +0200

    Change Log *Map Display Controller - getMap done! *MapManager - Hashmap maps done! - getMap done! - uploadMapsToDB in progress - updateMaps done *MapEditController - getAllNodes done! - getAllEdges done! - uploadMap in progress - addNode done! - deleteNode done! - editNode done! - addEdge done! - deleteEdge done! **Additional Changes *EdgeManager - added method getAllEdges (returns list of edges) *NodeManager - added method getAllNodes (returns list of nodes)

--- src/boundary/FXMLController.java
+++ src/boundary/FXMLController.java
@@ -1,22 +1,13 @@
 package boundary;
 
 import controller.MapDisplayController;
+import entity.MapManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class FXMLController {
-   final private MapDisplayController newMap = new MapDisplayController();
-
-   @FXML
-    private ScrollPane imageScroll;
-
-   @FXML
-   private void initialize(){
-       Image groundFloor= newMap.getMap("G");
-       imageScroll.setContent(new ImageView(groundFloor));
-   }
 
    public FXMLController() {
 
