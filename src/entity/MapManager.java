0cf801ea8a54009ebf80b7a1e69c947165e0be95
Quinn Averill <quinn.averill@trinetx.com>
2017-11-13 22:25:52 -0500

    Added getMap method

--- src/entity/MapManager.java
+++ src/entity/MapManager.java
@@ -1,13 +1,10 @@
 package entity;
 
+import javax.swing.*;
+import javax.swing.plaf.nimbus.State;
 import java.awt.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.io.*;
+import java.sql.*;
 import java.util.HashMap;
 
 public class MapManager {
@@ -22,8 +19,49 @@ public class MapManager {
      * @param floor The specified floor of desired map
      * @return The map of the specified floor
      */
-    public Image getMap(int floor){
-        return null;
+    public Image getMap(String floor) throws SQLException, IOException {
+        //Make the hash table of the floor's image path
+        HashMap<String, String> mapPaths = new HashMap<>();
+        mapPaths.put("L2", "src/boundary/images/Maps/00_thelowerlevel1.png");
+        mapPaths.put("L1", "src/boundary/images/Maps/00_thelowerlevel2.png");
+        mapPaths.put("G", "src/boundary/images/Maps/00_thegroundfloor.png");
+        mapPaths.put("1", "src/boundary/images/Maps/01_thefirstfloor.png");
+        mapPaths.put("2", "src/boundary/images/Maps/02_thesecondfloor.png");
+        mapPaths.put("3", "src/boundary/images/Maps/03_thethirdfloor.png");
+
+        //DB Info
+        String dbURL = "jdbc:derby://localhost:1527/bw_pathfinder_db;create=true;user=granite_gargoyle;password=wong";
+        final String driver = "org.apache.derby.jdbc.ClientDriver";
+
+        //Try to load drivers
+        try {
+            Class.forName(driver).newInstance();
+        }catch (Exception ex){
+            System.out.println("Failed to find Embedded JavaDB driver!");
+            ex.printStackTrace();
+        }
+
+        //Create connection and statement to be run
+        Connection conn = DriverManager.getConnection(dbURL);
+
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT image FROM map WHERE floor = " + floor);
+        InputStream in = rs.getBinaryStream(1);
+        OutputStream outputFile = null;
+        try {
+            outputFile = new FileOutputStream(new File(mapPaths.get(floor)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        int c = 0;
+        while ((c = in.read()) > -1) {
+            try {
+                outputFile.write(c);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return new ImageIcon(this.getClass().getResource(mapPaths.get(floor))).getImage();
     }
 
     /**
@@ -81,13 +119,4 @@ public class MapManager {
         }
 
     }
-
-    /**
-     * Updates the maps object to contain the latest maps in the database
-     */
-    public void updateMaps(){
-
-    }
-
-
 }
