021a1793f9b5af3e0dd4f4c669205cfbcca51b75
Sean Hunt <srhunt@wpi.edu>
2017-11-13 21:47:09 -0500

    Added method of getNearestNode() and testing... with a failed test do to null object created.

--- src/controller/ClickController.java
+++ src/controller/ClickController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import entity.Node;
+import entity.NodeManager;
+
 public class ClickController {
+
+    final private NodeManager nodemanager = new NodeManager();
+
+    public Node getNearestNode(int x, int y){
+        return nodemanager.nearestNode(x,y);
+    }
 }
