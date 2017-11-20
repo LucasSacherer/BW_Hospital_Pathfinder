c672458fe5e1dfc3e965b2e172ed2f79222c8fc6
Sean Hunt <srhunt@wpi.edu>
2017-11-20 00:36:12 -0500

    Created Getter methods for the subclasses of the MapNavigation package in the Facade class

--- src/MapNavigation/MapNavigationFacade.java
+++ src/MapNavigation/MapNavigationFacade.java
@@ -1,4 +1,41 @@
 package MapNavigation;
 
+import Entity.Node;
+import javafx.collections.ObservableList;
+import javafx.scene.image.Image;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+
 public class MapNavigationFacade {
+    private ClickController clickController;
+    private NearestPOIController nearestPOIController;
+    private MapDisplayController mapDisplayController;
+    private DirectoryController directoryController;
+
+    public MapNavigationFacade(){
+
+    }
+
+    public Node getNearestNode(int x, int y, String floor){
+        return clickController.getNearestNode(x,y,floor);
+    }
+
+    public Node getNearestPOI(int x, int y, String floor, String type){
+        return nearestPOIController.nearestPOI(x,y,floor,type);
+    }
+
+    public Image getFloorMap(String floor) throws IOException, SQLException{
+        return mapDisplayController.getMap(floor);
+    }
+
+    public HashMap<String,ObservableList<Node>> getDirectory(){
+        return directoryController.getDirectory();
+    }
+
+    public Node getDefaultNode(){
+        return directoryController.getDefaultNode();
+    }
+
 }
