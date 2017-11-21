cf05c03e81643d405338f4d3f988bbf165ffa10d
Lucas Sacherer <lsacherer@wpi.edu>
2017-11-21 11:32:36 -0500

    Added the image proxy

--- src/Entity/ImageProxy.java
+++ src/Entity/ImageProxy.java
@@ -4,12 +4,21 @@ package Entity;
 import javafx.scene.image.Image;
 
 public class ImageProxy {
-    private String path;
+    private final String path;
     private Image image;
-    private String floor;
+    private final String floor;
+
+    public ImageProxy(String path, String floor){
+        this.path = path;
+        this.floor = floor;
+        image = null;
+    }
 
     public Image getImage() {
-        return image;
+        if (image == null){
+            image = new Image(getClass().getResource(path).toString());
+        }
+        return  image;
     }
 
     private String getFloor() {
