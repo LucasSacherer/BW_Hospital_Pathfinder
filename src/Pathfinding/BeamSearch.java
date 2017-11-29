f14714d83041ad75d8417f6b5bebc6331d5f1fad
Ian MacGregor <iwmacgregor@wpi.edu>
2017-11-28 22:07:40 -0500

    Starting beam search

--- src/Pathfinding/BeamSearch.java
+++ src/Pathfinding/BeamSearch.java
@@ -1,10 +1,25 @@
 package Pathfinding;
+import Database.EdgeManager;
 import Entity.Node;
 
 import java.util.ArrayList;
+import java.util.List;
 
 class BeamSearch implements PathFinder {
     public ArrayList<Node> pathFind(Node start, Node end){
+        int beamWidth = 3;
+
+        EdgeManager edgeM;
+        // The set of nodes already evaluated
+        ArrayList<String> closedSet = new ArrayList<String>();
+        List<Node> neighbors = new ArrayList<Node>();
+        List<Node> connected = new ArrayList<Node>();
+        // The set of currently discovered nodes that are not evaluated yet.
+        boolean alreadfound = false;
+
+
+
         return null;
+
     }
 }
