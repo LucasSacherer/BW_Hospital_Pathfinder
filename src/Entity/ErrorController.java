d9620c5cd7072eee0c774f97aa834c9846ff7794
Melanie <mldworak@wpi.edu>
2017-12-06 00:05:35 -0500

    finished CSS

--- src/Entity/ErrorController.java
+++ src/Entity/ErrorController.java
@@ -3,6 +3,7 @@ package Entity;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.DialogPane;
+import javafx.stage.StageStyle;
 
 public class ErrorController {
 
@@ -12,17 +13,17 @@ public class ErrorController {
     public void showError(String errorMessage){
         Dialog error = new Dialog();
         error.setHeaderText("Error!");
-        error.setResizable(true);
+        error.setResizable(false);
         error.setContentText(errorMessage);
+        error.initStyle(StageStyle.UNDECORATED);
 
         DialogPane errorDialogPane = error.getDialogPane();
         errorDialogPane.getStylesheets().add(
                 getClass().getResource("/boundary/ErrorDialog.css").toExternalForm());
         errorDialogPane.getStyleClass().add("customError");
         errorDialogPane.getButtonTypes().add(ButtonType.CLOSE);
-        //TODO: implement an "ok" button with type CANCEL_CLOSE
+
         error.show();
-        System.out.print("showing error");
     }
 
 }
