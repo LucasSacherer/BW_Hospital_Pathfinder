4aa3ee48df79fdab2ab7e4ae8d4f19e1d44a7bd0
Quinn Averill <quinn.averill@trinetx.com>
2017-12-05 22:01:11 -0500

    started to create pathfinderlog manager

--- src/Database/PathfindingLogManager.java
+++ src/Database/PathfindingLogManager.java
@@ -1,11 +1,18 @@
 package Database;
 
 import DatabaseSetup.DatabaseGargoyle;
+import Entity.Node;
+
+import java.util.List;
 
 public class PathfindingLogManager {
     DatabaseGargoyle databaseGargoyle;
 
     public PathfindingLogManager(DatabaseGargoyle databaseGargoyle) {
         this.databaseGargoyle = databaseGargoyle;
+    }
+
+    public void addPathToLog(List<Node> path){
+
     }
 }
